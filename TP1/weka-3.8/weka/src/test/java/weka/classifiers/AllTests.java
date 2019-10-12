b7b8f2a07119a8e4b11b512d1ddc3debe31da2d7
Jérôme Désilets <jerome.desilets@gmail.com>
2019-10-12 14:03:20 -0400

    Removed additionnal useless tests

--- TP1/weka-3.8/weka/src/test/java/weka/classifiers/AllTests.java
+++ TP1/weka-3.8/weka/src/test/java/weka/classifiers/AllTests.java
@@ -38,8 +38,8 @@ public class AllTests
   public static Test suite() {
     TestSuite suite = new TestSuite();
 
-    suite.addTest(new TestSuite(weka.classifiers.CostMatrixTest.class));
-    suite.addTest(weka.classifiers.pmml.consumer.AllTests.suite());
+    //suite.addTest(new TestSuite(weka.classifiers.CostMatrixTest.class));
+    //suite.addTest(weka.classifiers.pmml.consumer.AllTests.suite());
     suite.addTest(suite("weka.classifiers.Classifier"));
     suite.addTest(suite("weka.classifiers.functions.supportVector.Kernel"));
 
