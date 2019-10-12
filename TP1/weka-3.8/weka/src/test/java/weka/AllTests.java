ecbff528f3dff3fa9f9c95a4ff79c8915b627e25
Jérôme Désilets <jerome.desilets@gmail.com>
2019-10-12 13:33:02 -0400

    Commented out unneeded test suites in AllTests.java

--- TP1/weka-3.8/weka/src/test/java/weka/AllTests.java
+++ TP1/weka-3.8/weka/src/test/java/weka/AllTests.java
@@ -33,13 +33,13 @@ public class AllTests extends TestSuite {
     TestSuite suite = new TestSuite();
 
     // Core components
-    suite.addTest(weka.core.AllTests.suite());
+    //suite.addTest(weka.core.AllTests.suite());
 
     // associators
     suite.addTest(weka.associations.AllTests.suite());
 
     // attribute selection
-    suite.addTest(weka.attributeSelection.AllTests.suite());
+    //suite.addTest(weka.attributeSelection.AllTests.suite());
 
     // classifiers
     suite.addTest(weka.classifiers.AllTests.suite());
@@ -48,13 +48,13 @@ public class AllTests extends TestSuite {
     suite.addTest(weka.clusterers.AllTests.suite());
 
     // data generators
-    suite.addTest(weka.datagenerators.AllTests.suite());
+    //suite.addTest(weka.datagenerators.AllTests.suite());
 
     // estimators
     // suite.addTest(weka.estimators.AllTests.suite());
 
     // filters
-    suite.addTest(weka.filters.AllTests.suite());
+    //suite.addTest(weka.filters.AllTests.suite());
 
     // High level applications
     // suite.addTest(weka.experiment.AllTests.suite());
